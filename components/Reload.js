5c360d8649e6680bd47229a99d7bb89769987342
Adam-CloudX <oosthuizen77011@gmail.com>
2020-12-02 13:56:46 +0200

    first commit

---
+++ components/Reload.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { View, Platform, StyleSheet } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import { colors } from "../utils/index";
+
+export default function Reload({ load }) {
+  const reloadIconName = Platform.OS === "ios" ? "ios-refresh" : "md-refresh";
+  return (
+    <View style={styles.reloadIcon}>
+      <Ionicons
+        onPress={load}
+        name={reloadIconName}
+        size={24}
+        color={colors.PRIMARY_COLOR}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  reloadIcon: {
+    position: "absolute",
+    ...Platform.select({
+      ios: {
+        top: -30,
+      },
+      android: {
+        top: 30,
+      },
+    }),
+    right: 20,
+    margin: 10,
+  },
+});
